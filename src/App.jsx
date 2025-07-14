19d9368aa6b1274759d04a38d3e09e165c29db5f
sagun basnet <basnetsagun8@gmail.com>
2025-07-14 12:39:27 +0545

    second routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
@@ -6,10 +7,86 @@ import ServicePage from "./pages/ServicePage";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import LoginPage from "./pages/LoginPage";
+
 const App = () => {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  // const DashboardLayout = () => {
+  //   return (
+  //     <>
+  //       <Topbar />
+  //       <div className="flex">
+  //         <SideBar />
+  //         <Outlet />
+  //       </div>
+  //     </>
+  //   );
+  // };
+
+  const pageRoute = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <HomePage />,
+        },
+        {
+          path: "/about",
+          element: <AboutPage />,
+        },
+        {
+          path: "/contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "/service",
+          element: <ServicePage />,
+        },
+      ],
+    },
+    // {
+    //   path: "/dashboard",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "",
+    //       element: <DashboardHome />,
+    //     },
+    //     {
+    //       path: "/dashboard/user-management",
+    //       element: <Usermanagement />,
+    //     },
+    //     {
+    //       path: "/dashboard/sales-management",
+    //       element: <SalesManagment />,
+    //     },
+    //   ],
+    // },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+
   return (
     <>
-      <Navbar />
+      <RouterProvider router={pageRoute} />
+      {/* <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
@@ -18,7 +95,7 @@ const App = () => {
         <Route path="/service" element={<ServicePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
